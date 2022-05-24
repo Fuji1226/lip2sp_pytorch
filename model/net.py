ae786b9ae3ecffbd2d1be99fa5a9fc61f9856cf0
Fuji1226 <tsfujiguuguru@gmail.com>
2022-05-24 15:43:15 +0900

    first

---
+++ model/net.py
@@ -0,0 +1,13 @@
+from torch import nn
+import torch
+from torch import nn
+
+
+####３DCNN+GAP#####
+class PreNet(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+    
+    def forward():
+        return
---
+++ train.py
@@ -0,0 +1,16 @@
+###ここにデータセットモデルのインスタンス作成
+
+
+def main():
+    ###ここにデータセットモデルのインスタンス作成train関数を回す#####
+    print('test')
+    return
+
+
+def train(model, dataloader, loss_function):
+    ###モデルにデータの入力、誤差の算出、逆伝搬によるパラメータの更新を行う####
+    return
+
+
+if __name__=='__main__':
+    main()
