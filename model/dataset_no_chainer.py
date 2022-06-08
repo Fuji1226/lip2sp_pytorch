61b9fd5bee4eaa91f2af7417c693b2a3c0b93c55
kyushusouth <minami.taisuke.326@s.kyushu-u.ac.jp>
2022-06-08 10:06:23 +0900

    chainerを使わない前処理を追加

---
+++ model/dataset_no_chainer.py
@@ -0,0 +1,358 @@
+"""
+reference
+https://github.com/Chris10M/Lip2Speech.git
+
+一旦データが出るようになりました
+"""
+
+import os
+import sys
+# 親ディレクトリからのimport用
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import random
+from pathlib import Path
+
+# brew install ffmpeg -> pip install ffmpeg-python
+import numpy as np
+from scipy.ndimage import gaussian_filter
+import torch
+import torch.nn as nn
+import torchvision
+from torchvision import transforms as T
+from torch.utils.data import Dataset, DataLoader
+
+# 自作
+from get_dir import get_datasetroot, get_data_directory
+from hparams import create_hparams
+from transform_no_chainer import load_data
+
+
+
+def get_datasets(data_root, mode):
+    """
+    train用とtest用のデータのディレクトリを分けておいて、modeで分岐させる感じにしてみました
+    とりあえず
+    dataset/lip/lip_cropped         にtrain用データ
+    dataset/lip/lip_cropped_test    にtest用データを適当に置いてやってみて動きました
+    """
+    items = dict()
+    idx = 0
+    if mode == "train":
+        for curDir, Dir, Files in os.walk(data_root):
+            for filename in Files:
+                # curDirの末尾がlip_croppedの時
+                if curDir.endswith("lip_cropped"):
+                    # filenameの末尾（拡張子）が.mp4の時
+                    if filename.endswith(".mp4"):
+                        format = ".mp4"
+                        video_path = os.path.join(curDir, filename)
+                        # 名前を同じにしているので拡張子だけ.wavに変更
+                        audio_path = os.path.join(curDir, filename.replace(str(format), ".wav"))
+
+                        if os.path.isfile(audio_path) and os.path.isfile(audio_path):
+                            items[idx] = [video_path, audio_path]
+                            idx += 1
+                else:
+                    continue
+    else:
+        for curDir, Dir, Files in os.walk(data_root):
+            for filename in Files:
+                # curDirの末尾がlip_cropped_testの時
+                if curDir.endswith("lip_cropped_test"):
+                    # filenameの末尾（拡張子）が.mp4の時
+                    if filename.endswith(".mp4"):
+                        format = ".mp4"
+                        video_path = os.path.join(curDir, filename)
+                        # 名前を同じにしているので拡張子だけ.wavに変更
+                        audio_path = os.path.join(curDir, filename.replace(str(format), ".wav"))
+
+                        if os.path.isfile(audio_path) and os.path.isfile(audio_path):
+                            items[idx] = [video_path, audio_path]
+                            idx += 1
+                else:
+                    continue
+    return items
+
+
+# def normalization(data_root=data_root, mode='train'):
+#     items = get_datasets(data_root, mode)
+#     data_len = len(items)
+#     item_iter = iter(items)
+#     item_idx = next(item_iter)
+
+#     while item_idx:
+#         video_path, audio_path = items[item_idx]
+
+
+def calc_mean_var(items, len, hparams):
+    lip_mean = 0
+    lip_std = 0
+    feat_mean = 0
+    feat_std = 0
+    feat_add_mean = 0
+    feat_add_std = 0
+    for i in range(len):
+        video_path, audio_path = items[i]
+        (lip, feature, feat_add, upsample), data_len = load_data(
+            data_path=Path(video_path),
+            gray=hparams.gray,
+            frame_period=hparams.frame_period,
+            feature_type=hparams.feature_type,
+            nmels=hparams.n_mel_channels,
+            f_min=hparams.f_min,
+            f_max=hparams.f_max,
+        )
+        
+        # 時間方向に平均と分散を計算
+        lip_mean += torch.mean(lip.float(), dim=(1, 2, 3))
+        lip_std += torch.mean(lip.float(), dim=(1, 2, 3))
+        feat_mean += torch.mean(feature, dim=0)
+        feat_std += torch.std(feature, dim=0)
+        feat_add_mean += torch.mean(feat_add, dim=0)
+        feat_add_std += torch.std(feat_add, dim=0)
+    
+    # データ全体の平均、分散を計算 (C,)
+    lip_mean /= len     
+    lip_std /= len      
+    feat_mean /= len    
+    feat_std /= len     
+    feat_add_mean /= len
+    feat_add_std /= len
+
+    return lip_mean, lip_std, feat_mean, feat_std, feat_add_mean, feat_add_std
+
+
+class KablabDataset(Dataset):
+    def __init__(self, data_root, transforms, hparams, mode='train',):
+        super().__init__()
+        assert mode in ('train', 'test'), "please set mode train or test"
+        self.data_root = data_root
+        self.transforms = transforms
+        self.hparams = hparams
+        self.mode = mode
+
+        # 口唇動画、音声データまでのパス一覧を取得
+        self.items = get_datasets(self.data_root, self.mode)
+        self.len = len(self.items)
+        
+        self.lip_mean, self.lip_std, self.feat_mean, self.feat_std, self.feat_add_mean, self.feat_add_std = calc_mean_var(self.items, self.len, self.hparams)
+        
+        print(f'Size of {type(self).__name__}: {self.len}')
+
+        # random.shuffle(self.items)
+
+    def __len__(self):
+        return self.len
+
+    def __getitem__(self, index):
+        #item_idx = next(self.item_iter)
+        video_path, audio_path = self.items[index]
+        data_path = Path(video_path)
+
+        ############################################
+        # data = (lip, y, feat_add, upsample)
+        # tensorで返してます
+        data, data_len = load_data(
+            data_path=Path(video_path),
+            gray=self.hparams.gray,
+            frame_period=self.hparams.frame_period,
+            feature_type=self.hparams.feature_type,
+            nmels=self.hparams.n_mel_channels,
+            f_min=self.hparams.f_min,
+            f_max=self.hparams.f_max,
+        )
+
+        data = self.transforms(
+            data, data_len, self.lip_mean, self.lip_std, self.feat_mean, self.feat_std, self.feat_add_mean, self.feat_add_std
+        )
+
+        lip = data[0]
+        feature = data[1]
+        feat_add = data[2]
+        #############################################
+
+
+        return lip, feature, feat_add, data_len
+
+
+class KablabTransform:
+    def __init__(self, length=300, delta=True):
+        # とりあえず適当に色々使ってみました
+        self.lip_transforms = T.Compose([
+            T.ColorJitter(brightness=[0.5, 1.5], contrast=0, saturation=1, hue=0.2),
+            T.GaussianBlur(kernel_size=(3, 3), sigma=(0.1, 5)),
+            T.RandomPerspective(distortion_scale=0.5, p=0.5),
+            T.RandomRotation(degrees=(0, 60)),
+            T.RandomPosterize(bits=3, p=0.5),
+            T.RandomAdjustSharpness(sharpness_factor=2, p=0.5),
+            T.RandomAutocontrast(p=0.5),
+            T.RandomEqualize(p=0.5),
+            T.RandomHorizontalFlip(p=0.5),
+        ])
+        self.length = length
+        self.delta = delta
+
+    def normalization(self, lip, feature, feat_add, lip_mean, lip_std, feat_mean, feat_std, feat_add_mean, feat_add_std):
+        """
+        正規化
+        """
+        lip = T.functional.normalize(lip.float(), lip_mean, lip_std)
+        # lip = (lip - lip_mean) / lip_std
+        feature = (feature - feat_mean) / feat_std
+        feat_add = (feat_add - feat_add_mean) / feat_add_std
+        return lip, feature, feat_add
+
+    def calc_delta(self, lip):
+        """
+        口唇動画の動的特徴量の計算
+        """
+        # scipywのgaussian_filterを使用するため、一旦numpyに戻してます
+        lip = lip.to('cpu').detach().numpy().copy()
+        if self.delta:
+            lip_pad = 0.30*lip[0:1] + 0.59*lip[1:2] + 0.11*lip[2:3]
+            lip_pad = lip_pad.astype(lip.dtype)
+            lip_pad = gaussian_filter(
+                lip_pad, (0, 0.5, 0.5, 0), mode="reflect", truncate=2)
+            lip_pad = np.pad(lip_pad, ((0, 0), (0, 0), (0, 0), (1, 1)), "edge")
+            lip_diff = (lip_pad[..., 2:] - lip_pad[..., :-2]) / 2
+            lip_acc = lip_pad[..., 0:-2] + \
+                lip_pad[..., 2:] - 2 * lip_pad[..., 1:-1]
+            lip = np.vstack((lip, lip_diff, lip_acc))
+            lip = torch.from_numpy(lip)
+        return lip
+
+    def time_adjust(self, lip, feature, feat_add, data_len, upsample):
+        """
+        フレーム数の調整
+        """
+        # data_lenが短い場合、0パディング
+        if data_len <= self.length:
+            # length分の0初期化
+            lip_padded = torch.zeros(lip.shape[0], lip.shape[1], lip.shape[2], self.length // upsample)
+            feature_padded = torch.zeros(self.length, feature.shape[1])
+            feat_add_padded = torch.zeros(self.length, feat_add.shape[1])
+
+            # 代入
+            for i in range(data_len // upsample):
+                lip_padded[..., i] = lip[..., i]
+            for i in range(data_len):
+                feature_padded[i, ...] = feature[..., i]
+                feat_add_padded[i, ...] = feat_add[i, ...]
+    
+            lip = lip_padded
+            feature = feature_padded
+            feat_add = feat_add_padded
+        
+        # data_lenが長い場合、ランダムにlengthだけ取り出す
+        if data_len > self.length:
+            # 開始時点をランダムに決める
+            idx = torch.div(torch.randint(0, int(data_len) - self.length, (1,)), upsample, rounding_mode="trunc")
+            # idx = torch.randint(0, int(data_len) - length, (1,)) // upsample
+
+            # length分の取り出し
+            lip = lip[..., idx:idx + torch.div(self.length, upsample, rounding_mode="trunc")]
+            # lip = lip[..., idx:idx + length // upsample]
+            feature = feature[idx * upsample:idx * upsample + self.length, :]
+            feat_add = feat_add[idx * upsample:idx * upsample + self.length, :]
+
+        assert lip.shape[-1] == torch.div(self.length, upsample, rounding_mode="trunc"), "lengthの調整に失敗しました"
+        assert feature.shape[0] and feat_add.shape[0] == self.length, "lengthの調整に失敗しました"
+        
+        return lip, feature, feat_add
+
+    def __call__(self, data, data_len, lip_mean, lip_std, feat_mean, feat_std, feat_add_mean, feat_add_std):
+        lip = data[0]
+        feature = data[1]
+        feat_add = data[2]
+        upsample = data[3]
+
+        # data augmentation
+        lip = lip.permute(-1, 0, 1, 2)  # (T, C, H, W)
+        lip = self.lip_transforms(lip)
+        
+        # normalization
+        lip, feature, feat_add = self.normalization(
+            lip, feature, feat_add, lip_mean, lip_std, feat_mean, feat_std, feat_add_mean, feat_add_std
+        )
+        lip = lip.permute(1, 2, 3, 0)   # (C, H, W, T)
+
+        # フレーム数の調整
+        lip, feature, feat_add = self.time_adjust(lip, feature, feat_add, data_len, upsample)
+
+        # 口唇動画の動的特徴量の計算
+        lip = self.calc_delta(lip)
+        
+        
+        return [lip, feature, feat_add]
+
+
+        
+
+def main():
+    print("############################ Start!! ############################")
+    data_root = Path(get_datasetroot()).expanduser()    
+    hparams = create_hparams()
+
+    trans = KablabTransform()
+
+    datasets = KablabDataset(
+        data_root=data_root,
+        transforms=trans,
+        hparams=hparams,
+        mode='train',
+    )
+    # datasets = KablabDataset(data_root, mode="test")
+    loader = DataLoader(
+        dataset=datasets,
+        batch_size=5,   
+        shuffle=True,
+        num_workers=0,      
+        pin_memory=False,
+        drop_last=True,
+    )
+
+    # print(datasets.__len__())
+    # print(datasets.current_item)
+    # print(datasets.reset_item())
+
+    # データ確認用。__getitem__(self, _)を__getitem__(self)に変更すれば見れます！
+    # ただ、そうすると下のresultsの方は見れません。どっちかだけです。
+    # frames, waveform, melspec = datasets.__getitem__()
+    # print("####### type #######")
+    # print(f"type(frames) = {type(frames)}")
+    # print(f"type(waveform) = {type(waveform)}")
+    # print(f"type(melspec) = {type(melspec)}")
+
+    # print("\n####### shape #######")
+    # print(f"frames.shape = {frames.shape}")
+    # print(f"waveform.shape = {waveform.shape}")
+    # print(f"melspec.shape = {melspec.shape}")
+
+    # print("\n####### len #######")
+    # print(f"len(frames) = {len(frames)}")
+    # print(f"len(waveform) = {len(waveform)}")
+    # print(f"len(melspec) = {len(melspec)}")
+
+
+    # results
+    for interation in range(1):
+        for bdx, batch in enumerate(loader):
+            lip, y, feat_add, data_len = batch
+            print("################################################")
+            print(type(lip))
+            print(type(y))
+            print(type(feat_add))
+            print(f"lip = {lip.shape}")  # (B, C=5, W=48, H=48, T=150)
+            print(f"y(acoustic features) = {y.shape}") # (B, C, T=300)
+            print(f"feat_add = {feat_add.shape}")     # (B, C=3, T=300)
+            print(f"data_len = {data_len}")
+
+
+
+
+
+
+if __name__ == "__main__":
+    # print(data_root)
+    main()
---
+++ transform_no_chainer.py
@@ -0,0 +1,183 @@
+"""
+load_dataのみに変更
+
+とりあえずchainerなくせました
+"""
+
+from skvideo.io import vread
+from pathlib import Path
+import cv2
+import numpy as np
+import torch
+import librosa
+from zipfile import BadZipFile
+from scipy.interpolate import interp1d
+from pysptk import swipe
+
+try:
+    from get_dir import get_data_directory
+    from utils import get_sp_name, get_upsample
+    from data_process.feature import wave2mel, wav2world
+    from hparams import create_hparams
+except:
+    from .get_dir import get_data_directory
+    from .utils import get_sp_name, get_upsample
+    from .data_process.feature import wave2mel, wav2world
+    from .hparams import create_hparams
+
+
+ROOT = Path(get_data_directory())
+
+
+def get_sp_path(name, path, data_root=ROOT, save_root=None, save_dir="sp"):
+    relpath = Path(path).relative_to(data_root)
+
+    if save_root is None:
+        save_root = data_root
+
+    world_dir = Path(save_root) / save_dir
+    ret_path = world_dir.joinpath(
+        *relpath.parent.parts[1:]) / (name + ".npy")
+
+    return ret_path
+
+
+def cals_sp(wave, fs, frame_period, feature_type, path=None, nmels=None, f_min=None, f_max=None):
+
+    loaded = False
+    if path is not None:
+        name = get_sp_name(path.stem, feature_type, frame_period, nmels)
+        load_path = get_sp_path(name, path)
+
+        try:
+            y = np.load(load_path, mmap_mode="r", allow_pickle=False)
+            loaded = True
+        except (FileNotFoundError, BadZipFile):
+            pass
+
+    if not loaded:
+        if feature_type == "mspec":
+            y = wave2mel(wave, fs, frame_period,
+                         n_mels=nmels, fmin=f_min, fmax=f_max).T
+
+        elif feature_type == "world":
+            # mcep, clf0, vuv, cap, _, fbin, _
+            # 元々こうなっていましたが、wav2worldの引数が6つでバグったので合わせました。
+            # 先輩のが謎です
+            mcep, clf0, vuv, cap, fbin, _ = wav2world(
+                wave, fs, frame_period=frame_period)
+    
+            y = np.hstack([mcep, clf0.reshape(-1, 1),
+                           vuv.reshape(-1, 1), cap])
+        else:
+            raise ValueError(feature_type)
+
+    return y
+
+
+def fill_nan(x):
+    flag = np.isfinite(x)
+    if flag.all():
+        return x
+
+    idx = np.arange(flag.size)
+    func = interp1d(
+        idx[flag], x[flag],
+        bounds_error=False,
+        fill_value=(x[flag][0], x[flag][-1])
+    )
+    x[~flag] = func(idx[~flag])
+
+    assert np.isfinite(x).all()
+    return x
+
+
+def continuous_f0(f0, amin=70):
+    f0[f0 < amin] = np.nan
+    vuv = np.isfinite(f0)
+    if np.isfinite(f0).sum() < 3:
+        f0[:] = amin
+    f0 = fill_nan(f0)
+    return f0, vuv
+
+
+def load_mp4(path, gray=False):
+    movie = cv2.VideoCapture(str(path))
+    fps = int(movie.get(cv2.CAP_PROP_FPS))
+    movie.release()
+
+    mov = vread(str(path), as_grey=gray, outputdict={"-s": "48x48"})
+    mov = np.asarray(mov).swapaxes(0, -1)
+
+    return mov, fps
+
+
+def load_data(data_path, gray, frame_period, feature_type, nmels, f_min, f_max, mode=None, delta=True):
+    """
+    先輩のコードはlabel, label_nameを取得していて、それをget_dvectorとかに使用している
+    おそらく捜索した時のファイル名とかを取ってくる感じだと思うのですが、一旦スルーしてます
+    lip2sp/submodules/mychainerutils/dataset.py 128行目からのPathDatasetクラスが関係してそうです
+
+    これは一旦使えそう
+    """
+    lip, fps = load_mp4(str(data_path), gray)
+    sppath = Path(data_path)
+    sppath = sppath.parent / (sppath.stem + ".wav")
+    wave, fs = librosa.load(str(sppath), sr=None, mono=None)
+    wave = wave[:int(lip.shape[-1]/fps*1.2*fs)]
+    # frame_period = 10
+    # feature_type = "mspec"
+    # nmels = 80
+    # f_min = 0
+    # f_max = 7600
+    upsample = get_upsample(fps, fs, frame_period)
+
+    # 音響特徴量への変換
+    y = cals_sp(
+        wave, fs, frame_period, feature_type,
+        path=data_path, nmels=nmels, f_min=f_min, f_max=f_max)
+    hop_length = fs * frame_period // 1000  # 160
+    
+    power = librosa.feature.rms(wave, frame_length=hop_length*2,
+                hop_length=hop_length).squeeze()
+    power = fill_nan(power)
+    f0 = swipe(wave.astype("float64"), fs, hop_length,
+               min=70.0, otype='f0').squeeze()
+
+    f0, vuv = continuous_f0(f0)
+    T = min(power.size, f0.size, y.shape[0])
+
+    feat_add = np.vstack((f0[:T], vuv[:T], power[:T])).T
+    feat_add = np.log(np.maximum(feat_add, 1.0e-7))
+    y = y[:T]
+
+    data_len = min(len(y) // upsample * upsample,  lip.shape[-1] * upsample)
+    y = y[:data_len]
+    feat_add = feat_add[:data_len]
+    lip = lip[..., :data_len // upsample]
+
+    lip = torch.from_numpy(lip)
+    y = torch.from_numpy(y)
+    feat_add = torch.from_numpy(feat_add)
+    data_len = torch.tensor(data_len)
+    upsample = torch.tensor(upsample)
+
+    ret = (
+        lip, y, feat_add, upsample
+    )
+
+    return_wave = False
+
+    if return_wave:
+        return wave, fs
+    else:
+        return ret, data_len
+
+
+
+
+
+# check
+if __name__ == "__main__":
+    data_path = Path('/Users/minami/dataset/lip/lip_cropped/ATR503_j05_0.mp4')
+    hparams = create_hparams()
