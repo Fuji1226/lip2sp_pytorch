f83616dd95a614e2a3569bbaa32fb67277bdbabe
kyushusouth <minami.taisuke.326@s.kyushu-u.ac.jp>
2022-06-21 23:05:24 +0900

    croppedからlip_croppedへwavをコピーする

---
+++ data_process/copy_wav.py
@@ -0,0 +1,29 @@
+"""
+croppedからlip_croppedへwavをコピーする
+norm.wavがよくわからないので，そこは無視して普通のwavだけをコピーしてます
+"""
+
+import shutil
+import os
+from pathlib import Path
+
+def main():
+    audio_path = "/home/usr4/r70264c/dataset/lip/cropped/F01_kablab"
+    save_path = "/home/usr4/r70264c/dataset/lip/lip_cropped/F01_kablab"
+    wavs = []
+    for curdir, dirs, files in os.walk(audio_path):
+        for file in files:
+            if file.endswith('.wav'):
+                if 'norm' in Path(file).stem:
+                    # normを回避
+                    continue
+                else:
+                    wavs.append(os.path.join(curdir, file))
+
+    for i in range(len(wavs)):
+        shutil.copy(wavs[i], save_path)
+
+
+if __name__ == "__main__":
+    main()
+
