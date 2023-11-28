610bbaa08d0a35ddff414c7a8a23123f34816d4d
Ryushimofmof <fujita.ryushi.340@s.kyushu-u.ac.jp>
2023-11-28 16:33:45 +0900

    1128ito

--- shells/generate.sh
+++ shells/generate.sh
@@ -22,7 +22,7 @@ module load cuda/11.0
 dir_project="$(dirname $(cd $(dirname $0); pwd))"
 # cd "${dir_project}/scripts"
 cd "${dir_project}"
-file_main="generate_realtime.py"
+file_main="generate_nar.py"
 
 # これより下に，Pythonなどを実行するコマンドを書きます。
 # 実際はもう少しごちゃごちゃした内容を記述していることが多いです。
