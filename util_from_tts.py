abfaa55a75b9d0e14572e1c8ffa3ecbc96bb05c7
naodesk <tsfujiguuguru@gmail.com>
2023-12-01 11:30:00 +0900

    add

--- util_from_tts.py
+++ util_from_tts.py
@@ -66,6 +66,50 @@ def load_from_vqvae(model, ckpt_path):
     return model
 
 
+        
+def load_from_vqvae_mlm(model, vq_path, mlm_path):
+    checkpoint = torch.load(str(vq_path))['model']
+    
+    save_module = ['decoder']
+    partial_state_dict = {}
+    for key, value in checkpoint.items():
+       for module in save_module:
+           if module in key:
+               key = key.replace(f'{module}.', '')
+               partial_state_dict[key] = value
+               break
+           
+    model.decoder.load_state_dict(partial_state_dict)
+
+    save_module = ['vq']
+    partial_state_dict = {}
+    for key, value in checkpoint.items():
+       for module in save_module:
+           if module in key:
+               key = key.replace(f'{module}.', '')
+               partial_state_dict[key] = value
+               break
+           
+    model.vq.load_state_dict(partial_state_dict, strict=False)
+    
+    checkpoint = torch.load(str(mlm_path))['model']
+    save_module = ['encoder']
+    partial_state_dict = {}
+    for key, value in checkpoint.items():
+       for module in save_module:
+           if module in key:
+               key = key.replace(f'{module}.', '')
+               partial_state_dict[key] = value
+               break
+    
+    
+    #model固定
+    for param in model.decoder.parameters():
+        param.requires_grad = False
+    for param in model.vq.parameters():
+        param.requires_grad = False
+    return model
+
 def load_lmmodel(model, lm_path):
     checkpoint = torch.load(str(lm_path))['model']
     
