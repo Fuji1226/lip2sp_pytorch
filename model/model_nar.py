0b5d0fbc0aac7659200d10a26cca8312ba20cfeb
Ryushimofmof <fujita.ryushi.340@s.kyushu-u.ac.jp>
2024-10-02 16:15:03 +0900

    241002_narnikumikomu

--- model/model_nar.py
+++ model/model_nar.py
@@ -14,6 +14,7 @@ from grad_reversal import GradientReversal
 from classifier import SpeakerClassifier
 from resnet18 import ResNet18
 from conformer.encoder import ConformerEncoder
+from avhubert import AVHuBERT
 
 
 class Lip2SP_NAR(nn.Module):
@@ -23,6 +24,7 @@ class Lip2SP_NAR(nn.Module):
         conf_n_layers, conf_n_head, conf_feedforward_expansion_factor,
         dec_n_layers, dec_kernel_size,
         n_speaker, spk_emb_dim,
+        cfg,
         which_encoder, which_decoder, where_spk_emb, use_spk_emb,
         dec_dropout, res_dropout, rnn_dropout, is_large, adversarial_learning, reduction_factor):
         super().__init__()
@@ -30,6 +32,7 @@ class Lip2SP_NAR(nn.Module):
         self.adversarial_learning = adversarial_learning
         inner_channels = int(res_inner_channels * 8)
 
+        #?resnetの選択
         if which_res == "default":
             self.ResNet_GAP = ResNet3D(
                 in_channels=in_channels, 
@@ -58,7 +61,10 @@ class Lip2SP_NAR(nn.Module):
                 hidden_channels=res_inner_channels,
                 dropout=res_dropout,
             )
+        #!elif which_res == "avhubert" :
 
+
+        #?エンコーダの選択
         if which_encoder == "transformer":
             self.encoder = Encoder(
                 n_layers=trans_n_layers, 
@@ -82,7 +88,13 @@ class Lip2SP_NAR(nn.Module):
                 num_attention_heads=conf_n_head,
                 feed_forward_expansion_factor=conf_feedforward_expansion_factor,
             )
+            #!↓要確認
+        elif which_encoder == "avhubert":
+            self.encoder = AVHuBERT(
+                cfg=cfg
+            )
 
+        #?話者特徴量
         if use_spk_emb:
             self.gr_layer = GradientReversal(1.0)
             self.classfier = SpeakerClassifier(
@@ -92,6 +104,7 @@ class Lip2SP_NAR(nn.Module):
             )
             self.spk_emb_layer = nn.Conv1d(inner_channels + spk_emb_dim, inner_channels, kernel_size=1)
 
+        #?デコーダの選択
         if which_decoder == 'restc':
             self.decoder = ResTCDecoder(
                 cond_channels=inner_channels,
